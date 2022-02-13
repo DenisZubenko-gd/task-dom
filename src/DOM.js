92f363a1d42376d3a168e842563acee60a2dd430
DenisZubenko-gd <75683015+DenisZubenko-gd@users.noreply.github.com>
2022-02-13 22:11:49 +0700

    task-dom

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,13 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    let i = count;
+    while (i > 0) {
+        --i;
+        let new_tag = document.createElement(tag);
+        new_tag.innerHTML = content;
+        document.body.append(new_tag);
+    }
 }
 
 /*
@@ -15,6 +22,21 @@ export function appendToBody(tag, content, count) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function generateTree(childrenCount, level) {
+    let new_div = document.createElement('div');
+    new_div.classList.add('item_1');
+    document.body.append(new_div);
+    for (let lev = 2; lev <= level; ++lev) {
+        let parent_dev = document.getElementsByClassName('item_' + (lev - 1));
+        for (let elem of parent_dev) {
+            let c = 0;
+            while (c < childrenCount) {
+                let new_div = document.createElement('div');
+                new_div.classList.add('item_' + lev);
+                elem.appendChild(new_div);
+                ++c;
+            }
+        }
+    }
 }
 
 /*
@@ -26,4 +48,13 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    generateTree(3, 3);
+    for (let elem of document.querySelectorAll('div.item_2')) {
+        let section = document.createElement('section');
+        section.innerHTML = elem.innerHTML;
+        elem.parentNode.replaceChild(section, elem);
+    }
+    for (let elem of document.getElementsByTagName('section')) {
+        elem.classList.add('item_2');
+    }
 }
